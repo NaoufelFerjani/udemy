3a9a47ef593e60c4531c1c92bae34de46b515c13
naoufel22 <ferjani.naoufel22@gmail.com>
2023-12-22 00:32:45 +0100

    Adding content to the recipes components & display recipe content with ngFor

--- recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,5 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Parmesan',
+      'A classic Italian dish, this is a hearty and flavorful meal',
+      'https://tastesbetterfromscratch.com/wp-content/uploads/2023/03/Chicken-Parmesan-1.jpg'
+    ),
+    new Recipe(
+      'Zucchini Noodles with Tomato Sauce',
+      "It's like pasta but made out",
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/4/5/0/RX-FNM_050111-WN-Dinners-008_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539886302.jpeg'
+    ),
+  ];
+  constructor() {}
 }
